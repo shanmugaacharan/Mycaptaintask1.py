21d3cef4efaf151375803e470777401d810c0a7b
shanmugaacharan <71343558+shanmugaacharan@users.noreply.github.com>
2021-04-19 18:56:36 +0530

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,11 @@
+mylist1=[12,-7,5,64,-14]
+mylist2=[12,14,-95,3]
+print("mylist1 output:")
+for ele in mylist1:
+    if ele >= 0:
+        print(ele, end=",")
+print("\n")
+print("mylist2 output:")
+for ele in mylist2:
+    if ele >= 0:
+        print(ele, end=",")
