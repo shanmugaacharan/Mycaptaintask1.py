eb6cecbda8662bc90b3b85abff1a08eafcfb66f9
shanmugaacharan <71343558+shanmugaacharan@users.noreply.github.com>
2021-04-19 19:07:53 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,8 +5,9 @@ sum=0
 count=1
 print("Fibonacci series:")
 while(count <= n):
-    print(sum, end=" ")
-    count+=1
     a = b
     b = sum
     sum = a + b
+    print(sum, end=" ")
+    count += 1
+
